a556a4aebfcdf2e867ce33a48d279b3d7745010d
Jarrod Sanders <jarrod.sanders@ufl.edu>
2022-02-10 22:28:46 -0500

    changes for the pi's current config

--- gps_data.py
+++ gps_data.py
@@ -13,17 +13,22 @@ def main(args):
         location = 'data'
         filename = 'sender.csv'
         filename = location + '/' + filename
+        
+        '''
         ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0',
         baudrate = 19200,
         parity = serial.PARITY_NONE, 
         stopbits = serial.STOPBITS_ONE,
         bytesize = serial.EIGHTBITS)
+        '''
 
-        uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
+        
+        uart = serial.Serial("/dev/serial1", baudrate=9600, timeout=10)
         gps = adafruit_gps.GPS(uart, debug=False)
         gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
         gps.send_command(b"PMTK220,1000")
         last_print = time.monotonic()
+        
 
   #time.sleep(5*60)
         while(True):
